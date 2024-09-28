170514612ebef51b9dd5d6d3619b7888cd3a7fb8
rajeshracha6 <rajeshracha752@gmail.com>
2024-09-28 12:55:40 +0530

    done

---
+++ js.js
@@ -0,0 +1,17 @@
+let arr = [1, 2, 3];
+arr.push(4); 
+console.log(arr); 
+
+let arr1 = [1, 2, 3, 4];
+let lastElement = arr1.pop(); 
+console.log(arr1); 
+console.log(lastElement); 
+
+let arr2 = [1, 2, 3, 4];
+let firstElement = arr2.shift(); 
+console.log(arr2); 
+console.log(firstElement);
+
+let arr3= [2, 3, 4];
+arr3.unshift(1); 
+console.log(arr3); 
